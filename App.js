c5042ad4bb3f92806f7c56d184f83053c444f08d
agregor2012 <flavio.leandrocezar11@gmail.com>
2026-07-10 20:33:12 -0300

    Ver 1.02 - Flavio Att

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
-      <ComponentFlavio></ComponentFlavio>
+      <ComponentFlavio/>
       <ComponentRaquelResende/>
     </View>
   );

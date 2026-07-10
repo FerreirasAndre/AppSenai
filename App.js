deb3d2e6b9e5d7cb5303e0b8dad2a80a46404eeb
agregor2012 <flavio.leandrocezar11@gmail.com>
2026-07-10 19:51:36 -0300

    Ver 1.01 - Flavio

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ComponentFlavio from './src/component/ComponentFlavio';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <ComponentFlavio></ComponentFlavio>
     </View>
   );
 }
---
+++ src/component/ComponentFlavio.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ComponentFlavio() {
+  return (
+    <View style={styles.container}>
+      <Text>"Hello World, My name is Flávio"</Text>
+    </View>
+  );
+}

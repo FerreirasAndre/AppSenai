abea7966e348c9b4ef92e15c0c97f6aea7a59101
agregor2012 <flavio.leandrocezar11@gmail.com>
2026-07-10 20:53:07 -0300

    ScrollView

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import ComponentFlavio from './src/component/ComponentFlavio';
 import ComponentRaquelResende from './src/component/ComponentRaqueResende';
 import ComponentCarlosHenrique from './src/component/ComponentCarlosHenrique';
+import { ScrollView } from 'react-native-web';
 
 export default function App() {
   return (
@@ -10,20 +11,38 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-<ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentFlavio/>
-      <ComponentRaquelResende/>
-      <ComponentCarlosHenrique/>
-      
+      <ScrollView>
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentFlavio />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentRaquelResende />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+        <ComponentCarlosHenrique />
+
+      </ScrollView>
     </View>
   );
 }

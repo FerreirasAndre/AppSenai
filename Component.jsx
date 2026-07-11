1db7c0435c0f631308b8f491edef4b053522e50a
Wanderson Lucas de Souza Ferreira <ben0050023@aluno.firjansenaisesi.com.br>
2026-07-10 21:04:45 -0300

    Meunome

---
+++ Component.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+
+
+export default function Component(){
+
+   return(
+    <View>
+        <Text>
+            Nome: Wanderson Lucas
+        </Text>
+    </View>
+   )
+
+}
+
